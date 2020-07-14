eb5dcc276fbeb297e1ea62602a5b3fd787cc63b0
Allon Mureinik <allon@synopsys.com>
2020-07-14 13:47:25 +0300

    Add LOG Injection demo

---
+++ logi.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const port = 3000;
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const urlencoded = bodyParser.urlencoded;
+const staticServer = express.static;
+
+app.use(urlencoded({ extended: true }));
+app.use('/', staticServer('./static/'));
+
+app.post('/logi', function(req, res) {
+    // We trust our users, every login will be successful!
+    const username = req.body.username;
+
+    // Enterprise-grade logging FTW!
+    console.log(username + ' logged in.');
+    res.end('Logged in with the honor system');
+})
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
