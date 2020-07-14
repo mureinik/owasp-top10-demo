c64138c9c1001b287808dfe612c3726eea392571
Allon Mureinik <allon@synopsys.com>
2020-07-14 20:48:07 +0300

    Add XXE example

---
+++ xxe.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const port = 3000;
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const libxmljs = require('libxmljs');
+
+app.use(bodyParser.text({type: '*/*'}));
+app.post('/xxe', function(req, res) {
+    const parsed = libxmljs.parseXml(req.body, {noent: true});
+    const name = parsed.get('//name').text();
+    res.end('Name is: ' + name);
+})
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
