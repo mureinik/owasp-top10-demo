5931ceab2f798dba534a71a1710806e37fbef275
Allon Mureinik <allon@synopsys.com>
2020-07-20 14:44:29 +0300

    Add example of insecure configuration (via code!)

---
+++ config.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const port = 3000;
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const expressSession = require('express-session');
+
+
+// app.use(helmet());
+app.use(bodyParser.json());
+app.use(expressSession());
+app.get('/example', function(req, res) {
+    res.end(`I'm in danger!`);
+});
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
